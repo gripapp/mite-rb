9080ed4f5675577151e2510298fd52028657aa86
Till Otto <till@t8lab.com>
2023-05-24 08:08:16 +0200

    Add the ability to get the account's details

--- lib/mite/account.rb
+++ lib/mite/account.rb
@@ -1,3 +1,10 @@
 class Mite::Account < Mite::SingletonBase
+  include Mite::ResourceWithoutWriteAccess
 
+  self.collection_name = 'account'
+
+  def self.my_account
+    account = (format.decode(connection.get(collection_path, headers).body) || {})
+    account ? instantiate_record(account) : nil
+  end
 end
